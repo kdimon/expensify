c79dc60dbf884ed707ae09ef47344f4b3fd17844
Dmitriy Kulik <d.kulik@youwe.nl>
2017-11-16 17:24:40 +0200

    Added react-redux

--- src/app.js
+++ src/app.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import getVisibleExpenses from './selectors/expenses';
+import {addExpense} from "./actions/expenses";
+import {setTextFilter} from "./actions/filters";
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const store = configureStore();
+
+store.dispatch(addExpense({description: 'Water bill'}));
+store.dispatch(addExpense({description: 'Gas bill'}));
+store.dispatch(setTextFilter('water'));
+
+setTimeout(() => {
+    store.dispatch(setTextFilter('rent'));
+}, 3000);
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(<AppRouter />, appRoot);
+ReactDOM.render(jsx, appRoot);
