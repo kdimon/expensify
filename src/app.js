05590ff517266f024e0a0c1fe776c87d66c797fc
Dmitriy Kulik <d.kulik@youwe.nl>
2017-11-17 17:29:55 +0200

    react + redux

--- src/app.js
+++ src/app.js
@@ -5,19 +5,14 @@ import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import getVisibleExpenses from './selectors/expenses';
 import {addExpense} from "./actions/expenses";
-import {setTextFilter} from "./actions/filters";
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'Water bill'}));
-store.dispatch(addExpense({description: 'Gas bill'}));
-store.dispatch(setTextFilter('water'));
-
-setTimeout(() => {
-    store.dispatch(setTextFilter('rent'));
-}, 3000);
+store.dispatch(addExpense({description: 'Water bill', amount: 4500}));
+store.dispatch(addExpense({description: 'Gas bill', createdAt: 1000}));
+store.dispatch(addExpense({description: 'Rent', amount: 10950}));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import moment from 'moment';
+import {SingleDatePicker} from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+
+export default class ExpenseForm extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            description: '',
+            note: '',
+            amount: '',
+            createdAt: moment(),
+            calendarFocused: false
+        };
+        this.onDescriptionChange = this.onDescriptionChange.bind(this);
+        this.onNoteChange = this.onNoteChange.bind(this);
+        this.onAmountChange = this.onAmountChange.bind(this);
+        this.onDateChange = this.onDateChange.bind(this);
+        this.onFocusChange = this.onFocusChange.bind(this);
+    }
+
+    onDescriptionChange(e) {
+        const description = e.target.value;
+        this.setState(() => ({description}));
+    }
+
+    onNoteChange(e) {
+        const note = e.target.value;
+        this.setState(() => ({note}));
+    }
+
+    onAmountChange(e) {
+        const amount = e.target.value;
+
+        if(amount.match(/^\d*(\.\d{0,2})?$/)) {
+            this.setState(() => ({amount}));
+        }
+    }
+
+    onDateChange(createdAt) {
+        this.setState(() => ({createdAt}));
+    }
+
+    onFocusChange({focused}) {
+        this.setState(() => ({calendarFocused: focused}));
+    }
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <input
+                        type='text'
+                        placeholder='Description'
+                        autoFocus
+                        value={this.state.description}
+                        onChange={this.onDescriptionChange}
+                    />
+                    <input
+                        type='text'
+                        placeholder='Amount'
+                        value={this.state.amount}
+                        onChange={this.onAmountChange}
+                    />
+                    <SingleDatePicker
+                        date={this.state.createdAt}
+                        onDateChange={this.onDateChange}
+                        focused={this.state.calendarFocused}
+                        onFocusChange={this.onFocusChange}
+                        numberOfMonths={1}
+                        isOutsideRange={() => false}
+                    />
+                    <textarea
+                        placeholder='Add a note for your expense (optional)'
+                        value={this.state.note}
+                        onChange={this.onNoteChange}
+                    >
+                    </textarea>
+                    <button>Add Expense</button>
+                </form>
+            </div>
+        );
+    }
+}
