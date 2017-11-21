e7a0ff949b53a4c375df38aa72dff369ff68d8a8
Dmitriy Kulik <d.kulik@youwe.nl>
2017-11-21 23:35:36 +0200

    added tests for whole app

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -3,7 +3,7 @@ import {DateRangePicker} from 'react-dates';
 import {connect} from 'react-redux';
 import {setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate} from "../actions/filters";
 
-class ExpenseListFilters extends React.Component {
+export class ExpenseListFilters extends React.Component {
     constructor() {
         super();
         this.state = {
@@ -11,15 +11,29 @@ class ExpenseListFilters extends React.Component {
         };
         this.onDatesChange = this.onDatesChange.bind(this);
         this.onFocusChange = this.onFocusChange.bind(this);
+        this.onTextChange = this.onTextChange.bind(this);
+        this.onSortChange = this.onSortChange.bind(this);
     }
 
     onDatesChange({startDate, endDate}) {
-        this.props.dispatch(setStartDate(startDate));
-        this.props.dispatch(setEndDate(endDate));
+        this.props.setStartDate(startDate);
+        this.props.setEndDate(endDate);
     };
 
     onFocusChange(focused) {
         this.setState(() => ({calendarFocused: focused}));
+    }
+
+    onTextChange(e) {
+        this.props.setTextFilter(e.target.value);
+    }
+
+    onSortChange(e) {
+        if(e.target.value === 'date') {
+            this.props.sortByDate();
+        }else if(e.target.value ==='amount') {
+            this.props.sortByAmount();
+        }
     }
 
     render() {
@@ -28,19 +42,11 @@ class ExpenseListFilters extends React.Component {
                 <input
                     type='text'
                     defaultValue={this.props.filters.text}
-                    onChange={e => {
-                        this.props.dispatch(setTextFilter(e.target.value));
-                    }}
+                    onChange={this.onTextChange}
                 />
                 <select
                     value={this.props.filters.sortBy}
-                    onChange={e => {
-                        if(e.target.value === 'date') {
-                            this.props.dispatch(sortByDate());
-                        }else if(e.target.value ==='amount') {
-                            this.props.dispatch(sortByAmount());
-                        }
-                    }}
+                    onChange={this.onSortChange}
                 >
                     <option value='date'>Date</option>
                     <option value='amount'>Amount</option>
@@ -60,10 +66,16 @@ class ExpenseListFilters extends React.Component {
     }
 }
 
-const mapStateToProps = state => {
-    return {
-        filters: state.filters
-    };
-};
+const mapStateToProps = state => ({
+    filters: state.filters
+});
 
-export default connect(mapStateToProps)(ExpenseListFilters);
+const mapDispatchToProps = dispatch => ({
+    setTextFilter: text => dispatch(setTextFilter(text)),
+    sortByDate: () => dispatch(sortByDate()),
+    sortByAmount: () => dispatch(sortByAmount()),
+    setStartDate: startDate => dispatch(setStartDate(startDate)),
+    setEndDate: endDate => dispatch(setStartDate(endDate))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ExpenseListFilters);
--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+import moment from 'moment';
+import {SingleDatePicker} from 'react-dates';
 import ExpenseForm from '../../components/ExpenseForm';
 import expenses from '../fixtures/expenses';
 
@@ -60,4 +62,37 @@ test('should set amount if invalid input', () => {
     wrapper.find('input').at(1).simulate('change', {target: {value}});
 
     expect(wrapper.state('amount')).toBe('');
+});
+
+test('should call onSubmit prop for valid form submission', () => {
+    const onSubmitSpy = jest.fn();
+    const wrapper = shallow(<ExpenseForm expense={expenses[0]} onSubmit={onSubmitSpy} />);
+
+    wrapper.find('form').simulate('submit', {preventDefault: () => {}});
+
+    expect(wrapper.state('error')).toBe('');
+    expect(onSubmitSpy).toHaveBeenLastCalledWith({
+        description: expenses[0].description,
+        amount: expenses[0].amount,
+        createdAt: expenses[0].createdAt,
+        note: expenses[0].note
+    });
+});
+
+test('should set new date on date change', () => {
+    const now = moment();
+    const wrapper = shallow(<ExpenseForm />);
+
+    wrapper.find(SingleDatePicker).prop('onDateChange')(now);
+
+    expect(wrapper.state('createdAt')).toEqual(now);
+});
+
+test('should set calendar focus on change', () => {
+    const focused = true;
+    const wrapper = shallow(<ExpenseForm />);
+
+    wrapper.find(SingleDatePicker).prop('onFocusChange')({focused});
+
+    expect(wrapper.state('calendarFocused')).toBe(focused);
 });
