40433e85ca3f97ccfbb6bdd9aa3a6ab2569e09b6
Dima Kulik <dimkakul@gmail.com>
2017-12-20 01:02:59 +0200

    Added tests for LoginPage

--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -2,16 +2,38 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import {LoginPage} from '../../components/LoginPage';
 
-test('should correctly render LoginPage', () => {
-    const wrapper = shallow(<LoginPage />);
+let startLogin,
+    wrapper;
 
+beforeEach(() => {
+    startLogin = jest.fn();
+    wrapper = shallow(<LoginPage startLogin={startLogin} />);
+});
+
+test('should correctly render LoginPage', () => {
     expect(wrapper).toMatchSnapshot();
 });
 
-test('should call startLogin on button click', () => {
-    const startLogin = jest.fn();
-    const wrapper = shallow(<LoginPage startLogin={startLogin} />);
-    wrapper.find('button').simulate('click');
+test('should call startLogin on google button click', () => {
+    wrapper.find('#google').simulate('click');
+
+    expect(startLogin).toHaveBeenCalled();
+});
+
+test('should call startLogin on facebook button click', () => {
+    wrapper.find('#facebook').simulate('click');
+
+    expect(startLogin).toHaveBeenCalled();
+});
+
+test('should call startLogin on github button click', () => {
+    wrapper.find('#github').simulate('click');
+
+    expect(startLogin).toHaveBeenCalled();
+});
+
+test('should call startLogin on twitter button click', () => {
+    wrapper.find('#twitter').simulate('click');
 
     expect(startLogin).toHaveBeenCalled();
 });
